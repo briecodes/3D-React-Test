eac3c3111b8bebce910a169ac3d9366ab5944f94
briecodes <hello@brie.work>
2018-06-18 11:27:03 -0400

    three.js added successfully via npm

--- src/components/video.js
+++ src/components/video.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import Webcam from 'react-webcam';
+// import Webcam from 'react-webcam';
+let THREE = require('three');
 
 class Video extends React.Component {
     render() {
-        // var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+    let camera, scene, renderer;
+    let geometry, material, mesh;
+ 
+    init();
+    animate();
+    
+    function init() {
+    
+        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+        camera.position.z = 1;
+    
+        scene = new THREE.Scene();
+    
+        geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        material = new THREE.MeshNormalMaterial();
+    
+        mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+    
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+    
+    }
+ 
+function animate() {
+ 
+    requestAnimationFrame( animate );
+ 
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+ 
+    renderer.render( scene, camera );
+ 
+}
+        // let colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
 
         // colors.on('track', function(event) {
         //     if (event.data.length === 0) {
@@ -18,7 +54,7 @@ class Video extends React.Component {
 
         return (
             <div id='test'>
-                <Webcam audio={false} height={350}/>
+                {/* <Webcam audio={false} height={350}/> */}
                 {/* <video id="myVideo" width="400" height="300" preload='true' autoPlay loop muted></video> */}
                 <button id="snap">Snap Photo</button>
                 <canvas id="canvas" width="640" height="480"></canvas>
